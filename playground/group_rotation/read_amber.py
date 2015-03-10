90d1f8c7ee93ec111acd474f7f546f9e9dff387d
Kyle <khs26@cam.ac.uk>
2015-03-10 01:02:43 +0000

    Miscellaneous changes suggested by PyCharm

--- playground/group_rotation/read_amber.py
+++ playground/group_rotation/read_amber.py
@@ -5,11 +5,13 @@ import re
 import playground.group_rotation.amino_acids as amino
 import pele.utils.elements as elem
 
+
 class AmberTopologyFile(object):
     """
     Python representation of the Amber topology file (.prmtop) described here:
     http://ambermd.org/formats.html
     """
+
     def __init__(self, filename=None):
         """
         Create the dictionaries and, if we provide a filename, run the read
@@ -18,8 +20,10 @@ class AmberTopologyFile(object):
         self.format_dict = {}
         self.data_dict = {}
         self.ordered_flags = []
+        self.input_filename = None
         if filename:
             self.read_from(filename)
+
     def read_from(self, filename):
         """
         Read from a .prmtop file to the AmberTopologyFile class.
@@ -62,10 +66,11 @@ class AmberTopologyFile(object):
                                           int(data_length))
             data_string = "".join(input_line[2:])
             # Loop through the data string, taking chunks as long as the data length each time.
-            split_string = [data_string[i:i+int(data_length)]
+            split_string = [data_string[i:i + int(data_length)]
                             for i in range(0, len(data_string), int(data_length))]
             # Store the parsed data in the data_dict.
             self.data_dict[flag] = [self.data_cast(data, data_type) for data in split_string]
+
     def write_to(self, filename):
         """
         Write to a .prmtop file from the AmberTopologyFile class.
@@ -113,11 +118,15 @@ class AmberTopologyFile(object):
                                           '.',
                                           str(format_tup[3]),
                                           'E}'])
+                else:
+                    raise ValueError
                 for line in regrouped:
                     data = filter(lambda x: x is not None, line)
                     output.write((format_one * len(data)).format(*data))
                     output.write('\n')
-    def data_cast(self, datum, type):
+
+    @staticmethod
+    def data_cast(datum, data_type):
         """
         Casts the datum as defined by type.
 
@@ -125,12 +134,13 @@ class AmberTopologyFile(object):
         i = integer
         e = float (in exponent notation)
         """
-        if type.lower() == "a":
+        if data_type.lower() == "a":
             return str(datum)
-        if type.lower() == "i":
+        if data_type.lower() == "i":
             return int(datum)
-        if type.lower() == "e":
+        if data_type.lower() == "e":
             return float(datum)
+
     def update_amber_pointers(self):
         """
         Updates self.data_dict["POINTERS"] to reflect any changes to the
@@ -177,17 +187,17 @@ class AmberTopologyFile(object):
         # Number of atom types
         self.data_dict["POINTERS"][1] = len(set(self.data_dict["ATOM_TYPE_INDEX"]))
         # Number of bonds containing hydrogen
-        self.data_dict["POINTERS"][2] = len(self.data_dict["BONDS_INC_HYDROGEN"])/3
+        self.data_dict["POINTERS"][2] = len(self.data_dict["BONDS_INC_HYDROGEN"]) / 3
         # Number of bonds not containing hydrogen
-        self.data_dict["POINTERS"][3] = len(self.data_dict["BONDS_WITHOUT_HYDROGEN"])/3
+        self.data_dict["POINTERS"][3] = len(self.data_dict["BONDS_WITHOUT_HYDROGEN"]) / 3
         # Number of angles containing hydrogen
-        self.data_dict["POINTERS"][4] = len(self.data_dict["ANGLES_INC_HYDROGEN"])/4
+        self.data_dict["POINTERS"][4] = len(self.data_dict["ANGLES_INC_HYDROGEN"]) / 4
         # Number of angles not containing hydrogen
-        self.data_dict["POINTERS"][5] = len(self.data_dict["ANGLES_WITHOUT_HYDROGEN"])/4
+        self.data_dict["POINTERS"][5] = len(self.data_dict["ANGLES_WITHOUT_HYDROGEN"]) / 4
         # Number of dihedrals containing hydrogen
-        self.data_dict["POINTERS"][6] = len(self.data_dict["DIHEDRALS_INC_HYDROGEN"])/5
+        self.data_dict["POINTERS"][6] = len(self.data_dict["DIHEDRALS_INC_HYDROGEN"]) / 5
         # Number of dihedrals not containing hydrogen
-        self.data_dict["POINTERS"][7] = len(self.data_dict["DIHEDRALS_WITHOUT_HYDROGEN"])/5
+        self.data_dict["POINTERS"][7] = len(self.data_dict["DIHEDRALS_WITHOUT_HYDROGEN"]) / 5
         # Number of excluded atoms
         self.data_dict["POINTERS"][10] = sum(self.data_dict["NUMBER_EXCLUDED_ATOMS"])
         # Number of residues
@@ -213,8 +223,10 @@ class AmberTopologyFile(object):
         res_ends = self.data_dict["RESIDUE_POINTER"][1:] + [self.data_dict["POINTERS"][0]]
         self.data_dict["POINTERS"][28] = max([y - x for x, y in zip(res_starts, res_ends)])
 
+
 class Atom(object):
     """ Atom defined from the AMBER topology file. """
+
     def __init__(self, index, name, mass, amber_atom_type, charge, molecule):
         self.index = index
         self.name = name.strip()
@@ -222,23 +234,31 @@ class Atom(object):
         self.amber_atom_type = amber_atom_type
         self.charge = charge
         self.residue = None
-        self.set_element(mass)
+        self.element = self.identify_element(self.mass)
         self.molecule = molecule
-    def set_element(self, mass):
+
+    @staticmethod
+    def identify_element(mass):
         """ Defines the element, based on the mass. """
-        self.element = elem.lookup_element_by_mass(mass)
+        return elem.lookup_element_by_mass(mass)
+
     def set_residue(self, residue):
         """ Defines the Residue to which the Atom belongs. """
         self.residue = residue
+
     def __repr__(self):
         return " ".join([str(self.index), self.element, self.name])
 
+
 class Residue(object):
     """ Residue defined from the AMBER topology file. """
+
     def __init__(self, index, name, molecule):
         self.index = index
         self.name = name.strip()
         self.molecule = molecule
+        self.atoms = None
+
     def add_atoms(self, atoms):
         """ 
         Adds Atoms in atoms to the Residue and sets the residue for those
@@ -247,13 +267,17 @@ class Residue(object):
         self.atoms = atoms
         for atom in atoms:
             atom.set_residue(self)
+
     def __repr__(self):
         return str(self.index) + " " + self.name
-            
+
+
 class Molecule(object):
     """ Molecule defined from the AMBER topology file. """
+
     def __init__(self):
         self.atoms = nx.Graph()
+
 
 def create_atoms_and_residues(topology_data):
     """
@@ -265,24 +289,24 @@ def create_atoms_and_residues(topology_data):
     # The first element of POINTERS is the atom count.
 
     atoms = list(it.imap(Atom,
-                                range(0, (topology_data["POINTERS"][0])),
-                                topology_data["ATOM_NAME"],
-                                topology_data["MASS"],
-                                topology_data["AMBER_ATOM_TYPE"],
-                                topology_data["CHARGE"],
-                                it.repeat(molecule)))
+                         range(0, (topology_data["POINTERS"][0])),
+                         topology_data["ATOM_NAME"],
+                         topology_data["MASS"],
+                         topology_data["AMBER_ATOM_TYPE"],
+                         topology_data["CHARGE"],
+                         it.repeat(molecule)))
     molecule.atoms.add_nodes_from(atoms)
     # Create a list of residues and add them to the molecule's list of residues.
     # The 11th element of POINTERS is the residue count.
     residues = list(it.imap(Residue,
-                                   range(0, (topology_data["POINTERS"][11])),
-                                   topology_data["RESIDUE_LABEL"],
-                                   it.repeat(molecule)))
+                            range(0, (topology_data["POINTERS"][11])),
+                            topology_data["RESIDUE_LABEL"],
+                            it.repeat(molecule)))
     molecule.residues.add_nodes_from(residues)
     # Go through the BONDS_INC_HYDROGEN and BONDS_WITHOUT_HYDROGEN lists to extract lists of bonded atoms.
     # Index is i / 3 + 1, because AMBER still uses coordinate indices for runtime speed.
-    first_atoms  = map(lambda x: (x / 3) + 1,
-                       topology_data["BONDS_INC_HYDROGEN"][0::3] + topology_data["BONDS_WITHOUT_HYDROGEN"][0::3])
+    first_atoms = map(lambda x: (x / 3) + 1,
+                      topology_data["BONDS_INC_HYDROGEN"][0::3] + topology_data["BONDS_WITHOUT_HYDROGEN"][0::3])
     second_atoms = map(lambda x: (x / 3) + 1,
                        topology_data["BONDS_INC_HYDROGEN"][1::3] + topology_data["BONDS_WITHOUT_HYDROGEN"][1::3])
     bond_list = zip(first_atoms, second_atoms)
@@ -292,17 +316,18 @@ def create_atoms_and_residues(topology_data):
     for i, residue in enumerate(residues):
         try:
             start = residue_indices[i] - 1
-            end = residue_indices[i+1] - 1
-        except IndexError, ex:            
+            end = residue_indices[i + 1] - 1
+        except IndexError:
             end = None
         residue.add_atoms(atoms[start:end])
     # Now go through bond list and create bonds between the relevant atoms.
     for bond in bond_list:
-        bonded_atoms = (atoms[(bond[0]-1)], atoms[(bond[1]-1)])
+        bonded_atoms = (atoms[(bond[0] - 1)], atoms[(bond[1] - 1)])
         molecule.atoms.add_edge(*bonded_atoms)
         if bonded_atoms[0].residue != bonded_atoms[1].residue:
             molecule.residues.add_edge(bonded_atoms[0].residue, bonded_atoms[1].residue)
     return molecule
+
 
 def group_rotation_file(molecule, params, filename):
     """
@@ -327,11 +352,11 @@ def group_rotation_file(molecule, params, filename):
                 if residue.name == param[0]:
                     rotated_atoms = get_rotated_atoms((residue, param[1]))
                     # Create an identifiable group name consisting of [index]_[res_name]_[atom_1]_[atom_2]
-                    group_name = "_".join((str(residue.index), 
-                                           residue.name.strip(), 
+                    group_name = "_".join((str(residue.index),
+                                           residue.name.strip(),
                                            rotated_atoms[0].name.strip(),
                                            rotated_atoms[1].name.strip()))
-                    output.write(" ".join(("GROUP", 
+                    output.write(" ".join(("GROUP",
                                            group_name,
                                            str(rotated_atoms[0].index + 1),
                                            str(rotated_atoms[1].index + 1),
@@ -343,6 +368,7 @@ def group_rotation_file(molecule, params, filename):
                         output.write(str(atom.index + 1) + "\n")
                     output.write("\n")
 
+
 def group_rotation_dict(molecule, params):
     groups = {}
     for param in params.keys():
@@ -350,8 +376,8 @@ def group_rotation_dict(molecule, params):
             if residue.name == param[0]:
                 rotated_atoms = get_rotated_atoms((residue, param[1]))
                 # Create an identifiable group name consisting of [index]_[res_name]_[atom_1]_[atom_2]
-                group_name = "_".join((str(residue.index), 
-                                       residue.name.strip(), 
+                group_name = "_".join((str(residue.index),
+                                       residue.name.strip(),
                                        rotated_atoms[0].name.strip(),
                                        rotated_atoms[1].name.strip()))
                 groups[group_name] = {}
@@ -361,7 +387,8 @@ def group_rotation_dict(molecule, params):
                 groups[group_name]["max_angle_magnitude"] = params[param][0]
                 groups[group_name]["selection_probability"] = params[param][1]
     return groups
-                    
+
+
 def get_rotated_atoms(bond):
     """
     Returns a list of the atoms in the rotating group for the GROUPROTATION script.
@@ -388,36 +415,39 @@ def get_rotated_atoms(bond):
         atom_1 = atom_a
         atom_2 = atom_b
         rotating_atoms.remove(atom_b)
-    return (atom_1, atom_2, rotating_atoms)
+    return atom_1, atom_2, rotating_atoms
+
 
 def read_amber_coords(filename):
     field_length = 12
     coords = []
     with open(filename, "r") as coords_file:
-    # Throw away the first line, since it just contains the name of the molecule.
+        # Throw away the first line, since it just contains the name of the molecule.
         coords_file.readline()
-    # The next line contains the number of atoms.
+        # The next line contains the number of atoms.
         number_of_atoms = int(coords_file.readline())
-    # Later lines contain coordinates in 12-character wide fields.
+        # Later lines contain coordinates in 12-character wide fields.
         for line in coords_file:
             line = line.rstrip()
-            coords += map(float, [line[i:i+field_length] for i in range(0, len(line), field_length)])
+            coords += map(float, [line[i:i + field_length] for i in range(0, len(line), field_length)])
     # If the number of coordinates is not equal to 3 * number of atoms, raise a RuntimeError. 
     if len(coords) != number_of_atoms * 3:
         raise ex.RuntimeError("Number of coordinates in coords file and number of atoms are inconsistent.")
     return coords
 
+
 def default_parameters(topology_filename):
-    topology_data = read_topology(topology_filename)
+    topology_data = AmberTopologyFile(topology_filename)
     parsed = create_atoms_and_residues(topology_data)
     return group_rotation_dict(parsed, amino.def_parameters)
 
+
 if __name__ == "__main__":
     # import sys
-    topology_data = AmberTopologyFile('../amber/coords.prmtop')
-    topology_data.write_to('dummy')
+    test_top_file = AmberTopologyFile('../amber/coords.prmtop')
+    test_top_file.write_to('dummy')
     # for k, v in topology_data.data_dict.items():
-    #     print k, ":", v
+    # print k, ":", v
     # for k, v in topology_data.format_dict.items():
     #     print k, ":", v
     # parsed = create_atoms_and_residues(topology_data)
