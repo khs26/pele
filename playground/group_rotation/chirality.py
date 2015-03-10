f6fc436426f656600c7aacda6a3b06339d0035c6
Kyle <khs26@cam.ac.uk>
2015-03-10 00:54:25 +0000

    Reformatted the chirality file.

--- playground/group_rotation/chirality.py
+++ playground/group_rotation/chirality.py
@@ -4,40 +4,43 @@ import read_amber as ra
 import networkx as nx
 import sys
 
-class GhostAtom(ra.Atom):
-  pass
 
-valences = { "H": 1,
-         "C": 4,
-         "N": 3,
-         "O": 2,
-         "S": 2,
-         "Se": 2,
-         "P": 5 }
+class GhostAtom(ra.Atom):
+    pass
+
+
+valences = {"H": 1,
+            "C": 4,
+            "N": 3,
+            "O": 2,
+            "S": 2,
+            "Se": 2,
+            "P": 5}
+
 
 def chiral_candidates(atoms):
-  candidates = [atom for atom in atoms.nodes() if len(nx.edges(atoms, atom)) == 4]
-  return candidates
+    candidates = [atom for atom in atoms.nodes() if len(nx.edges(atoms, atom)) == 4]
+    return candidates
+
 
 def multi_bonds(atoms):
-  multibonded = [atom for atom in atoms.nodes() 
-                 if len(nx.edges(atoms, atom)) < valences[atom.element]]
-  for i, atom in enumerate(multibonded):
-    paired = False
-    for other in atoms.neighbors(atom):
-      if isinstance(other, GhostAtom):
-        paired = True
-        continue
-      if len(nx.edges(atoms, other)) < valences[other.element]:
-        ghost_atom = GhostAtom(**(atom.__dict__))
-        ghost_atom.name = atom.name + "*"
-        ghost_other = GhostAtom(**(other.__dict__))
-        ghost_other.name = other.name + "*"
-        atoms.add_edge(other, ghost_atom)
-        atoms.add_edge(atom, ghost_other)
-        paired = True
-#    if not paired:
-#      print "Could not find pair for atom", atom, atom.residue, atom.charge, nx.edges(atoms, atom)
+    multibonded = [atom for atom in atoms.nodes()
+                   if len(nx.edges(atoms, atom)) < valences[atom.element]]
+    for i, atom in enumerate(multibonded):
+        paired = False
+        for other in atoms.neighbors(atom):
+            if isinstance(other, GhostAtom):
+                paired = True
+                continue
+            if len(nx.edges(atoms, other)) < valences[other.element]:
+                ghost_atom = GhostAtom(**(atom.__dict__))
+                ghost_atom.name = atom.name + "*"
+                ghost_other = GhostAtom(**(other.__dict__))
+                ghost_other.name = other.name + "*"
+                atoms.add_edge(other, ghost_atom)
+                atoms.add_edge(atom, ghost_other)
+                paired = True
+
 
 def chiral_order(atoms, chiral_atom, depth=6):
     # print "\n\nResidue:", chiral_atom.residue, "atom:", chiral_atom
@@ -50,7 +53,8 @@ def chiral_order(atoms, chiral_atom, depth=6):
     if len(hydrogens) < 3:
         tree = nx.bfs_tree(atoms, chiral_atom)
         # Generate the list of shortest paths in the molecule, neglecting the trivial path [chiral_atom]
-        paths = sorted(nx.single_source_shortest_path(tree, chiral_atom, depth).values(), reverse=True, key=lambda x: map(lambda at: at.mass, x))[:-1]
+        paths = sorted(nx.single_source_shortest_path(tree, chiral_atom, depth).values(), reverse=True,
+                       key=lambda x: map(lambda at: at.mass, x))[:-1]
         while paths:
             # Pop the first element (highest priority path) from the list of paths and remove any duplicates.
             path = paths.pop(0)
@@ -75,22 +79,25 @@ def chiral_order(atoms, chiral_atom, depth=6):
         ordered += [atom for atom in neighbors if atom.element == "H"]
     return ordered
 
+
 def write_chirality_file(input_filename, output_filename):
-  molecule = ra.parse_topology_file(input_filename)
-  atoms = molecule.atoms
-  chiral_cands = chiral_candidates(atoms)
-  multi_bonds(atoms)
-  chiral_centres = {}
-  for i, chiral_atom in enumerate(chiral_cands):
-    ordered = chiral_order(atoms, chiral_atom)
-    if len(ordered) == 4:
-      chiral_centres[chiral_atom] = ordered
-  with open(output_filename, "w") as output_file:
-    for atom in sorted(chiral_centres.keys(), cmp=lambda x, y: cmp(x.index, y.index)):
-    # Write out the list of chiral atoms and their CIP-ranked neighbours.
-      output_string = "{0:>8d}{1:>8d}{2:>8d}{3:>8d}{4:>8d}\n".format(atom.index + 1, 
-                                                                     *[other_atom.index + 1 for other_atom in chiral_centres[atom]])
-      output_file.write(output_string)
+    molecule = ra.parse_topology_file(input_filename)
+    atoms = molecule.atoms
+    chiral_cands = chiral_candidates(atoms)
+    multi_bonds(atoms)
+    chiral_centres = {}
+    for i, chiral_atom in enumerate(chiral_cands):
+        ordered = chiral_order(atoms, chiral_atom)
+        if len(ordered) == 4:
+            chiral_centres[chiral_atom] = ordered
+    with open(output_filename, "w") as output_file:
+        for atom in sorted(chiral_centres.keys(), cmp=lambda x, y: cmp(x.index, y.index)):
+            # Write out the list of chiral atoms and their CIP-ranked neighbours.
+            output_string = "{0:>8d}{1:>8d}{2:>8d}{3:>8d}{4:>8d}\n".format(atom.index + 1,
+                                                                           *[other_atom.index + 1 for other_atom in
+                                                                             chiral_centres[atom]])
+            output_file.write(output_string)
+
 
 if __name__ == "__main__":
-  write_chirality_file(sys.argv[1], ".chirality_list")
+    write_chirality_file(sys.argv[1], ".chirality_list")
