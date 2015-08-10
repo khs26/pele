1671926789f0bc425e3ad63a272b7f3c78d1840c
Kyle Sutherland-Cash <khs26@cam.ac.uk>
2015-08-10 18:05:43 +0100

    Commented out old function and tested new dihedral measurement functions.

--- playground/rotamer/find_dihedrals.py
+++ playground/rotamer/find_dihedrals.py
@@ -5,38 +5,38 @@ from identify_residue import find_sidechains, residue_from_sidechain
 import networkx as nx
 
 
-def expand_dihedral(atom1, atom2):
-    """ Finds the appropriate neighbouring atoms for atom1 and atom2, to define the dihedral properly.
-
-    This first tries to find the atom with the highest rank according to the Cahn-Ingold-Prelog rules for determining
-    chirality. If there is a tie (e.g. valine, leucine, tyrosine), it instead takes one of the equivalent atoms and
-    notes the symmetry of the atoms.
-
-    :param atom1: atom 1 of pair
-    :param atom2: atom 2 of pair
-    :return:
-    """
-    atoms = atom1.molecule.atoms
-    sym0, sym3 = False, False
-    # Try the chirality test
-    try:
-        ordered = [i for i in chir.chiral_order(atoms, atom1) if i != atom2]
-        atom0 = ordered[0]
-        if len(ordered) < 3:
-            sym0 = True
-    except IndexError:
-        atom0 = sorted(nx.neighbors(atoms, atom1), key=lambda x: x.mass)[-1]
-    try:
-        ordered = [i for i in chir.chiral_order(atoms, atom2) if i != atom1]
-        atom3 = ordered[0]
-        if "VAL" in atom3.residue.name:
-            print "Ordered:", ordered
-            exit()
-        if len(ordered) < 3:
-            sym3 = True
-    except IndexError:
-        atom3 = sorted(nx.neighbors(atoms, atom2), key=lambda x: x.mass)[-1]
-    return atom0, atom1, atom2, atom3, sym0, sym3
+# def expand_dihedral(atom1, atom2):
+#     """ Finds the appropriate neighbouring atoms for atom1 and atom2, to define the dihedral properly.
+#
+#     This first tries to find the atom with the highest rank according to the Cahn-Ingold-Prelog rules for determining
+#     chirality. If there is a tie (e.g. valine, leucine, tyrosine), it instead takes one of the equivalent atoms and
+#     notes the symmetry of the atoms.
+#
+#     :param atom1: atom 1 of pair
+#     :param atom2: atom 2 of pair
+#     :return:
+#     """
+#     atoms = atom1.molecule.atoms
+#     sym0, sym3 = False, False
+#     # Try the chirality test
+#     try:
+#         ordered = [i for i in chir.chiral_order(atoms, atom1) if i != atom2]
+#         atom0 = ordered[0]
+#         if len(ordered) < 3:
+#             sym0 = True
+#     except IndexError:
+#         atom0 = sorted(nx.neighbors(atoms, atom1), key=lambda x: x.mass)[-1]
+#     try:
+#         ordered = [i for i in chir.chiral_order(atoms, atom2) if i != atom1]
+#         atom3 = ordered[0]
+#         if "VAL" in atom3.residue.name:
+#             print "Ordered:", ordered
+#             exit()
+#         if len(ordered) < 3:
+#             sym3 = True
+#     except IndexError:
+#         atom3 = sorted(nx.neighbors(atoms, atom2), key=lambda x: x.mass)[-1]
+#     return atom0, atom1, atom2, atom3, sym0, sym3
 
 
 def map_dihedrals(residue_identities, residue_atom_map):
@@ -70,7 +70,7 @@ def map_dihedrals(residue_identities, residue_atom_map):
 if __name__ == "__main__":
     import os.path
     import numpy as np
-    from playground.rotamer.measure_dihedral import dihedral_angle
+    from playground.rotamer.measure_dihedral import dihedrals_with_symmetry
 
     topology_data = amber.read_topology(os.path.normpath("/home/khs26/flu.prmtop"))
     coords = np.array(amber.read_amber_coords(os.path.normpath("/home/khs26/flu.inpcrd"))).reshape((-1, 3))
@@ -81,4 +81,4 @@ if __name__ == "__main__":
     print ress
     print maps
     for k, v in map_dihedrals(ress, maps).items():
-        print k, [(dihedral, dihedral_angle(coords, dihedral) * 180.0 / np.pi) for dihedral in v]
+        print k, dihedrals_with_symmetry(coords, k, ress, v)
