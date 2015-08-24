e1f1b907d550dea023e86444c990f92d2f2e285e
Kyle Sutherland-Cash <khs26@cam.ac.uk>
2015-08-24 18:05:26 +0100

    Testing of dihedrals with symmetry and initial setting of dihedral values for output to GMIN.

--- playground/rotamer/find_dihedrals.py
+++ playground/rotamer/find_dihedrals.py
@@ -1,30 +1,51 @@
 import pele.amber.read_amber as amber
 import networkx as nx
 import playground.group_rotation.chirality as chirality
-from playground.rotamer.measure_dihedral import dihedral_with_symmetry
+import playground.group_rotation.transforms as transforms
+from playground.rotamer.measure_dihedral import dihedral_with_symmetry, symmetric_atoms
 
 class Dihedral(object):
     def __init__(self, dihedral_atoms):
         self.atoms = dihedral_atoms
         self.residue = dihedral_atoms[0].residue
         self.atom_graph = dihedral_atoms[0].molecule.atoms
+        # self.atom_map = {v: k for k, v in self.residue.atom_map.items()}
+        self.atom_map = self.residue.atom_map
         self.get_moving_atoms()
     def measure_dihedral(self, coords):
         return dihedral_with_symmetry(coords, self)
-    def change_dihedral(self, start_coords):
-        current_angle = self.measure_dihedral(start_coords)
+    def set_dihedral(self, coords, value):
+        """ Returns coordinates which set the dihedral to a particular angle, value.
+
+        :param coords: Coordinates of the system
+        :param value: Target angle for the dihedral (-pi < x < pi)
+        :return new_coords: New coordinates (with chosen dihedral angle)
+        """
+        current_angle = self.measure_dihedral(coords)
+        rotation_angle = value - current_angle
+        # Define one end of the axis and the axis
+        coords_1 = coords[self.atoms[1].index]
+        bond_axis = coords[self.atoms[2].index] - coords_1
+        # Create the relevant AffineTransform object
+        trans_to_origin = transforms.translation(-1.0 * coords_1)
+        trans_back = transforms.translation(+1.0 * coords_1)
+        rotation = transforms.proper_rotation(bond_axis,
+                                              rotation_angle,
+                                              affine=True)
+        overall = trans_back * rotation * trans_to_origin
+        # Apply the transform to our coordinates
+
     def get_moving_atoms(self):
         """ Returns a list of moving atoms for a given dihedral.
 
-        :param dihedral: List of atoms which define the dihedral.
-        :param atom_graph: Atom graph.
         :return moving_atoms: list of moving atoms.
         """
-        self.atom_graph.remove_edge(dihedral[1], dihedral[2])
-        moving_atoms = [g for g in nx.connected_components(self.atom_graph) if dihedral[2] in g][0]
+        self.atom_graph.remove_edge(self.atoms[1], self.atoms[2])
+        moving_atoms = [g for g in nx.connected_components(self.atom_graph) if self.atoms[2] in g][0]
         self.moving_atoms = [atom for atom in moving_atoms if not isinstance(atom, chirality.GhostAtom)]
-        self.atom_graph.add_edge(dihedral[1], dihedral[2])
+        self.atom_graph.add_edge(self.atoms[1], self.atoms[2])
         return self.moving_atoms
+
 
 
 def map_dihedrals(residue_identities, residue_atom_map):
@@ -41,7 +62,7 @@ def map_dihedrals(residue_identities, residue_atom_map):
     for residue, res_id in residue_identities.items():
         # If the residue doesn't have an entry in dihedral_chain, skip to the next residue.
         if not dihedral_chain[res_id]:
-            print "Skipping:", residue, res_id
+            # print "Skipping:", residue, res_id
             continue
         unmapped_dihedrals = [dihedral_chain[res_id][i:i+4] for i in range(0, len(dihedral_chain[res_id]) - 3)]
         mapped_dihedrals = []
@@ -60,8 +81,8 @@ if __name__ == "__main__":
     from playground.rotamer.measure_dihedral import dihedrals_with_symmetry
     import cProfile
 
-    pr = cProfile.Profile()
-    pr.enable()
+    # pr = cProfile.Profile()
+    # pr.enable()
     topology_data = amber.read_topology(os.path.normpath("/home/khs26/flu.prmtop"))
     coords = np.array(amber.read_amber_coords(os.path.normpath("/home/khs26/flu.inpcrd"))).reshape((-1, 3))
     molecule = amber.create_molecule(topology_data)
@@ -70,8 +91,9 @@ if __name__ == "__main__":
     for v in map_dihedrals(ress, maps).values():
         for dihedral in v:
             dihedrals.append(Dihedral(dihedral))
-    for dihe in dihedrals:
-        print dihe.residue, dihe.atoms, dihe.moving_atoms
-        print "Angle:", dihe.measure_dihedral(coords)
-    pr.disable()
-    pr.print_stats('cumulative')
+    for dihe in sorted(dihedrals, key=lambda x: x.residue.index):
+        if dihe.residue.identity in symmetric_atoms:
+            print dihe.residue, dihe.atoms, dihe.residue.atoms
+            print "Angle:", dihe.measure_dihedral(coords) * 180.0 / np.pi
+    # pr.disable()
+    # pr.print_stats('cumulative')
